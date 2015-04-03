18474648d2c474a2d7dd298774a9125018df7ad9
Abdallah Khaled <dinopedia93@gmail.com>
2015-04-03 13:49:02 +0200

    calculating rating for training

--- app/Model/UsersRatingTrainer.php
+++ app/Model/UsersRatingTrainer.php
@@ -18,10 +18,26 @@ class UsersRatingTrainer extends AppModel {
 			    'rating' => $new_rating
 			));
 		}
+		$this->CalculateRating($trainer_id);
 	}
 
-	function CalculateRating($trainer_id)
+	private function CalculateRating($trainer_id)
 	{
+		$rating_one_count = $this->find('count',array('conditions' => array('UsersRatingTrainer.trainer_id' => $trainer_id,'UsersRatingTrainer.rating' => 1)));
+		$rating_two_count = $this->find('count',array('conditions' => array('UsersRatingTrainer.trainer_id' => $trainer_id,'UsersRatingTrainer.rating' => 2)));
+		$rating_three_count = $this->find('count',array('conditions' => array('UsersRatingTrainer.trainer_id' => $trainer_id,'UsersRatingTrainer.rating' => 3)));
+		$rating_four_count = $this->find('count',array('conditions' => array('UsersRatingTrainer.trainer_id' => $trainer_id,'UsersRatingTrainer.rating' => 4)));
+		$rating_five_count = $this->find('count',array('conditions' => array('UsersRatingTrainer.trainer_id' => $trainer_id,'UsersRatingTrainer.rating' => 5)));
+
+		$rating = ( ($rating_one_count) + (2 * $rating_two_count) + (3 * $rating_three_count) + (4 * $rating_four_count) + (5 * $rating_five_count))/($rating_one_count + $rating_two_count + $rating_three_count + $rating_four_count + $rating_five_count);
 		
+		// Load model in other model
+		$TrainerModel = ClassRegistry::init('Trainer');
+
+		$TrainerModel->set(array(
+		    'rank' => $rating
+		));
+		$TrainerModel->id = $trainer_id;
+		$TrainerModel->save();
 	}
 }
